18f17663451b57ca6e7f102c1d25f1a3ee147430
aidaramirov <aidaramirov@mail.ru>
2021-04-16 00:38:17 +0300

    Added delete(item) and bugged add

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -6,22 +6,23 @@ import { Task } from '../models/taskItem';
 import { TASKS } from '../mock-tasks'
 import { map } from 'rxjs/operators';
 
- 
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+  // @ts-ignore
   private tasks$:BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>(TASKS);
   done:boolean = false;     //
-  
-  constructor() { 
+
+  constructor() {
   }
 
   getTasks(){
     return this.tasks$;
   }
- 
+
   getTask(id:number | string){
     return this.getTasks().pipe(
       map(tasks=>tasks.find(task=>task.id===+id))
@@ -30,6 +31,17 @@ export class TaskService {
 
   addTask(id:number,name:string,desc:string,time:string){
     TASKS.push(new Task(id,name,desc,time));
+    // @ts-ignore
     this.tasks$.next(TASKS);
   }
+
+  deleteTask(task:Task){
+    this.tasks$.getValue().splice(task.id,1);
+  }
+  makeTaskImportant(task:Task){
+    return (this.getTask(task.id)).toPromise
+  }
+
+
+
 }
